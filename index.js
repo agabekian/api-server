22271f35f3c4b702986a1bffbbe2d953dde823a1
Armen Agabekian <2008nv@gmail.com>
2024-05-01 16:11:14 -0700

    configured-sequalize

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+require('dotenv').config();
+
+const server = require('./src/server.js');
+const { db } = require('./src/models');
+
+db.sync()
+  .then( () => {
+    server.start(process.env.PORT);
+  })
+  .catch(console.error);
