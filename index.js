e9b938751ebb0d7e9cffcbc8c1db61c44a416b9e
Armen Agabekian <2008nv@gmail.com>
2024-05-01 20:28:47 -0700

    connecivity-ok

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
-
-require('dotenv').config();
+const path = require('path') //needed that sadly
+require('dotenv').config({path: path.resolve(__dirname, '.env')})
 
 const server = require('./src/server.js');
-const { db } = require('./src/models');
+const {db} = require('./src/models');
 
 db.sync()
-  .then( () => {
-    server.start(process.env.PORT);
-  })
-  .catch(console.error);
+    .then(() => {
+        console.log(`running in ${process.env.NODE_ENV} at ${process.env.DATABASE_URL}- ${process.env.TEST_STRING}`);
+        server.start(process.env.PORT);
+    })
+    .catch(console.error);
--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,19 @@
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory' : process.env.DATABASE_URL;
 
-const {Sequelize, DataTypes } = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
 
-let sequelize = new Sequelize(DATABASE_URL, {logging:false});
+// I fixed that  .env path !
+//remote string in .env works as well , just use DATABASE_URL as above
+
+// const POSTGRES_URI = 'postgres://localhost' //LOCAL tests
+
+// let sequelize = new Sequelize(DATABASE_URL, {logging:false}); //old?
+// const sequelize = new Sequlize('dialect://connection.string');
+
+let sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false
+});
 
 const peopleModel = require('./people.js');
 
